839723a412f802e0218b574ee2a9ef881e2edc98
kakcalu13 <kevin.a.araujo@gmail.com>
2024-07-11 16:49:45 -0400

    updated to latest

--- embodiments/neuraville/feagi_connector_video_capture/feagi_connector_video_capture/__main__.py
+++ embodiments/neuraville/feagi_connector_video_capture/feagi_connector_video_capture/__main__.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 import argparse
-import subprocess
-import sys
-import os
-import sysconfig
-import feagi_connector_video_capture
-import traceback
 import requests
 from time import sleep
-from feagi_connector_video_capture.configuration import *
+import feagi_connector_video_capture
+from feagi_connector import feagi_interface as feagi
 
 if __name__ == '__main__':
     # Check if feagi_connector has arg
+    current_path = feagi_connector_video_capture.__path__
+    config = feagi.build_up_from_configuration(str(current_path[0]) + '/')
+    feagi_settings = config['feagi_settings'].copy()
+    agent_settings = config['agent_settings'].copy()
+    default_capabilities = config['default_capabilities'].copy()
+    message_to_feagi = config['message_to_feagi'].copy()
+    capabilities = config['capabilities'].copy()
+
+
     parser = argparse.ArgumentParser(description='configuration for any webcam')
     parser.add_argument('-loop', '--loop', help='Enable loop for the video', required=False)
     parser.add_argument('-ip', '--ip', help='Description for ip address argument', required=False)
