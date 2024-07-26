1657cb15414222e33d45ca6419366799313fafca
Kat <kat.peace@neuraville.com>
2024-07-26 10:27:22 -0400

    comment out constant send

--- embodiments/neuraville/feagi_trainer/controller.py
+++ embodiments/neuraville/feagi_trainer/controller.py
@@ -105,8 +105,8 @@ if __name__ == "__main__":
                         rgb, capabilities, False) # processes visual data into FEAGI-comprehensible form
 
                 # Process the image to show FEAGI selection boxes on image in browser
-                if len(default_capabilities['hardcode_for_kat']) > 0:
-                    process_image(default_capabilities['hardcode_for_kat'])
+                # if len(default_capabilities['hardcode_for_kat']) > 0:
+                #     process_image(default_capabilities['hardcode_for_kat'])
 
                 # If camera data is available, generate data for FEAGI
                 if 'camera' in rgb: # This is the data wrapped for feagi data to read
