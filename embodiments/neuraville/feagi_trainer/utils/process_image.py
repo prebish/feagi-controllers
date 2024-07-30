35b8e43e9640aa32a7791ea942bb969259e6d7a9
kakcalu13 <kevin.a.araujo@gmail.com>
2024-07-30 16:27:16 -0400

    clean up

--- embodiments/neuraville/feagi_trainer/utils/process_image.py
+++ embodiments/neuraville/feagi_trainer/utils/process_image.py
@@ -44,14 +44,10 @@ def process_image(image, location_data=None):
     original_height, original_width = copied_frame.shape[:2]
     aspect_ratio = original_width / original_height
     target_height = int(target_width / aspect_ratio)
-    
     resized_frame = cv2.resize(copied_frame, (target_width, target_height))
 
     if location_data:
-        # Get coordinates from location_data
         top_left, bottom_right = get_corners(location_data, original_size=(32, 32), target_size=resized_frame.shape[1::-1])
-        # top_left = (160, 60)
-        # bottom_right = (400, 240)
 
         # Define the border thickness
         border_thickness = 3
@@ -65,28 +61,7 @@ def process_image(image, location_data=None):
         # Draw the inner green rectangle
         cv2.rectangle(resized_frame, top_left, bottom_right, (0, 255, 0), 2)
 
-        # Add text to the image in the top left of the box
-        # text = "FEAGI Perception: dog"
-        # text_position = (top_left[0], top_left[1] + 5)  # Adjusted to fit background
-
-        # # Font and text size
-        # font = cv2.FONT_HERSHEY_SIMPLEX
-        # font_scale = 0.6
-        # font_thickness = 2
-
-        # # Calculate the width and height of the text box
-        # (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, font_thickness)
-        # text_height += baseline
-
-        # # Create a rectangle filled with black color for the text background
-        # background_top_left = (text_position[0], text_position[1] - text_height)
-        # background_bottom_right = (text_position[0] + text_width, text_position[1] + baseline)
-        # cv2.rectangle(resized_frame, background_top_left, background_bottom_right, (0, 0, 0), cv2.FILLED)
-
-        # # Put the text over the black rectangle
-        # cv2.putText(resized_frame, text, text_position, font, font_scale, (0, 255, 0), font_thickness)
 
     # Save the processed image to a file
     image_path = 'utils/latest_image.jpg'
     cv2.imwrite(image_path, resized_frame)
-    # print("Image updated")
