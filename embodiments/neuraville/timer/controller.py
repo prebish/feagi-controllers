dcdc647a7807ff3981a73d539aeb832f58c69b7b
kakcalu13 <kevin.a.araujo@gmail.com>
2024-08-20 13:49:39 -0400

    create a genome function to load

--- embodiments/neuraville/timer/controller.py
+++ embodiments/neuraville/timer/controller.py
@@ -33,6 +33,60 @@ previous_frame_data = {}
 def current_milli_time():
     return time.time() * 1000
 
+def prepare_the_genome_of_timer(args):
+    # Prepare the file to be uploaded
+    print("Loading genome now...")
+    with open('test_timer.json', 'rb') as f:
+        files = {'file': f}
+
+        # Send the POST request with the file
+        url = api_address + '/v1/genome/upload/file'
+        response = requests.post(url, files=files)
+
+    flag = True
+    print(response.status_code)
+    while flag:
+        # Print the response
+        if response.status_code == 200:
+            # print("genome loaded")
+            flag = False
+        else:
+            # print("failed. type: ", type(response.status_code)," and status: ", response.status_code)
+            sleep(1)
+
+
+    json_data = {"cortical_id": "CM0Tim", "cortical_name": "Timer",
+                 "cortical_dimensions": [int(args['number']), 100, 1]}
+    url = api_address + '/v1/cortical_area/cortical_area'
+    response = requests.put(url, json=json_data)
+    flag = True
+    while flag:
+        # Print the response
+        if response.status_code == 200:
+            # print("updated timer cortical")
+            flag = False
+        else:
+            # print("failed. type: ", type(response.status_code)," and status: ", response.status_code)
+            response = requests.put(url, json=json_data)
+            sleep(3)
+
+    json_data = {"cortical_id": "o_misc", "cortical_name": "Miscellaneous",
+                 "cortical_dimensions_per_device": [int(args['number']), 1, 1]}
+    url = api_address + '/v1/cortical_area/cortical_area'
+    response = requests.put(url, json=json_data)
+    flag = True
+    while flag:
+        # Print the response
+        if response.status_code == 200:
+            # print("updated misc cortical")
+            flag = False
+        else:
+            # print("failed. type: ", type(response.status_code)," and status: ", response.status_code)
+            response = requests.put(url, json=json_data)
+            sleep(3)
+    print("genome is completed")
+
+
 
 if __name__ == '__main__':
     feagi_settings, agent_settings, capabilities, message_to_feagi, configuration = FEAGI.configuration_load(
@@ -48,63 +102,13 @@ if __name__ == '__main__':
                                capabilities, __version__)
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # - - - - - - - - - - -
-
-    # Prepare the file to be uploaded
-    with open('test_timer.json', 'rb') as f:
-        files = {'file': f}
-
-        # Send the POST request with the file
-        url = api_address + '/v1/genome/upload/file'
-        response = requests.post(url, files=files)
-
-    flag = True
-    print(response.status_code)
-    while flag:
-        # Print the response
-        if response.status_code == 200:
-            print("genome loaded")
-            flag = False
-        else:
-            print("failed. type: ", type(response.status_code),
-                  " and status: ", response.status_code)
-            sleep(1)
-
     args = vars(parser.parse_args())
-    json_data = {"cortical_id": "CM0Tim", "cortical_name": "Timer",
-                 "cortical_dimensions": [int(args['number']), 100, 1]}
-    url = api_address + '/v1/cortical_area/cortical_area'
-    response = requests.put(url, json=json_data)
-    flag = True
-    while flag:
-        # Print the response
-        if response.status_code == 200:
-            print("updated timer cortical")
-            flag = False
-        else:
-            print("failed. type: ", type(response.status_code),
-                  " and status: ", response.status_code)
-            response = requests.put(url, json=json_data)
-            sleep(3)
-
-    json_data = {"cortical_id": "o_misc", "cortical_name": "Miscellaneous",
-                 "cortical_dimensions_per_device": [int(args['number']), 1, 1]}
-    url = api_address + '/v1/cortical_area/cortical_area'
-    response = requests.put(url, json=json_data)
-    flag = True
-    while flag:
-        # Print the response
-        if response.status_code == 200:
-            print("updated misc cortical")
-            flag = False
-        else:
-            print("failed. type: ", type(response.status_code),
-                  " and status: ", response.status_code)
-            response = requests.put(url, json=json_data)
-            sleep(3)
+    prepare_the_genome_of_timer()
     message_to_feagi = sensors.add_generic_input_to_feagi_data(
         {'i_misc': {"0-0-0": 100}}, message_to_feagi)
     pns.signals_to_feagi(message_to_feagi, feagi_ipu_channel, agent_settings,
                          feagi_settings)
+
     start_time = current_milli_time()
     previous_total_time = start_time
     iteration_list = []
@@ -137,6 +141,7 @@ if __name__ == '__main__':
                     iteration_list = []
                     if flag:
                         break
+                    prepare_the_genome_of_timer()
                     json_data = {"global_visualization": False}
                     url = api_address + '/v1/system/global_activity_visualization'
                     response = requests.put(url, json=json_data)
@@ -144,8 +149,7 @@ if __name__ == '__main__':
                     flag = True
             if current_milli_time() - start_time >= 2000.0 and not iteration_list:
                 print("missed, trying again...")
-                message_to_feagi = sensors.add_generic_input_to_feagi_data(
-                    {'i_misc': {"0-0-0": 100}}, message_to_feagi)
+                message_to_feagi = sensors.add_generic_input_to_feagi_data({'i_misc': {"0-0-0": 100}}, message_to_feagi)
                 previous_total_time = current_milli_time()
 
 
