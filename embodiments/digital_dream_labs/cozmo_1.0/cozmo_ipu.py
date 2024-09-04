0d8012b96c2958ad281082ac6b8a93d822401a33
kakcalu13 <kevin.a.araujo@gmail.com>
2024-09-04 17:56:23 -0400

    hotfix remove customized code

--- embodiments/digital_dream_labs/cozmo_1.0/cozmo_ipu.py
+++ embodiments/digital_dream_labs/cozmo_1.0/cozmo_ipu.py
@@ -16,28 +16,4 @@ def cozmo_ipu(robot, capabilities, angle_of_head, angle_of_arms, message_to_feag
     
     if robot['proximity']:
         message_to_feagi = sensors.create_data_for_feagi('proximity', capabilities, message_to_feagi, robot['proximity'][0], symmetric=True, measure_enable=True)
-    
-    if pns.full_template_information_corticals:
-        # TODO: Figure how to fix this. Also I need to get the actual servo, not my own code tracker
-        cortical_id = pns.name_to_feagi_id(sensor_name='servo')
-        for device_id in capabilities['input']['servo']:
-            raw_data = 0
-            if device_id == '0':
-                raw_data = angle_of_head
-            if device_id == '1':
-                raw_data = angle_of_arms
-            if not capabilities['input']['servo'][device_id]['disabled']:
-                create_data_list = dict()
-                create_data_list[cortical_id] = dict()
-                position_in_feagi_location = sensors.convert_sensor_to_ipu_data(
-                    capabilities['input']['servo'][device_id]['min_value'],
-                    capabilities['input']['servo'][device_id]['max_value'],
-                    raw_data,
-                    capabilities['input']['servo'][device_id]['feagi_index'],
-                    sensor_name='servo',
-                    symmetric=True)
-                create_data_list[cortical_id][position_in_feagi_location] = 100
-                if create_data_list[cortical_id]:
-                    message_to_feagi = sensors.add_generic_input_to_feagi_data(create_data_list,
-                                                                               message_to_feagi)
     return message_to_feagi
