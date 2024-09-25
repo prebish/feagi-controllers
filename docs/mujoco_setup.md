64bfb6622718485335f9ab7eb66b488f8e1597bb
Joel Prebish <prebish.j@outlook.com>
2024-09-25 14:38:32 -0400

    including mujoco files

--- docs/mujoco_setup.md
+++ docs/mujoco_setup.md
@@ -5,3 +5,6 @@
 python -m mujoco.viewer --mjcf=.\mujoco\model\humanoid\humanoid.xml
 
 Make sure you run this command while in the root directory.
+
+# MuJoCo Documentation link:
+https://mujoco.readthedocs.io/en/stable/APIreference/APItypes.html#mjmodel
---
+++ src/main.py
@@ -0,0 +1,46 @@
+import time, random
+
+import mujoco
+import mujoco.viewer
+
+m = mujoco.MjModel.from_xml_path('mujoco\model\humanoid\humanoid.xml')
+d = mujoco.MjData(m)
+
+inc_flag: bool = True # init flags for inc and dec in mujoco loop
+dec_flag: bool = False
+tolerance = 0.01  # Small tolerance for floating-point comparison
+
+with mujoco.viewer.launch_passive(m, d) as viewer:
+  # Close the viewer automatically after 30 wall-seconds.
+  start = time.time()
+  while viewer.is_running() and time.time() - start < 30:
+    step_start = time.time()
+
+    # make model spaz out
+    for i in range(len(d.ctrl)):
+      d.ctrl[i] += random.randint(0, 2)
+      d.ctrl[i] -= random.randint(0, 2)
+      if d.ctrl[i] > 4: d.ctrl = 0
+    # if d.ctrl[0] < 1 and inc_flag:
+    #     d.ctrl[0] += 0.001  # Increment the value
+        
+    # print(m.njnt)
+
+    # mj_step can be replaced with code that also evaluates
+    # a policy and applies a control signal before stepping the physics.
+    mujoco.mj_step(m, d)
+
+    # Example modification of a viewer option: toggle contact points every two seconds.
+    # with viewer.lock():
+    #   viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
+
+    # Pick up changes to the physics state, apply perturbations, update options from GUI.
+    viewer.sync()
+
+    # Rudimentary time keeping, will drift relative to wall clock.
+    time_until_next_step = m.opt.timestep - (time.time() - step_start)
+    if time_until_next_step > 0:
+      time.sleep(time_until_next_step)
+
+
+
