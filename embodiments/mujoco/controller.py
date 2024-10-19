4545889e84a34f4a2b4a7fa4374a8f346d0e6535
ctd-pitt <cmt135@pitt.edu>
2024-10-19 11:07:14 -0400

    gyro

--- embodiments/mujoco/controller.py
+++ embodiments/mujoco/controller.py
@@ -47,9 +47,24 @@ def action(obtained_data, capabilities):
     """
     recieve_servo_data = actuators.get_servo_data(obtained_data)
     recieve_servo_position_data = actuators.get_servo_position_data(obtained_data)
+
+    print("obtained data string: %s", obtained_data)
+    print("obtained data d: %d", obtained_data)
+
+    """ recieve_gyro_data = actuators.get_gyro_data(obtained_data)
+
+    if recieve_gyro_data:
+        print("gyro data: %d", recieve_gyro_data)
+        # example output: {0: 0.245, 2: 1.0}
+            for real_id in recieve_gyro_data:
+            servo_number = real_id
+            new_power = recieve_servo_data[real_id]
+            data.ctrl[servo_number] = new_power """
+    
     
     if recieve_servo_position_data:
         # output like {0:0.50, 1:0.20, 2:0.30} # example but the data comes from your capabilities' servo range
+        #print("hello1")
         for real_id in recieve_servo_position_data:
             servo_number = real_id
             new_power = recieve_servo_position_data[real_id]
@@ -57,6 +72,7 @@ def action(obtained_data, capabilities):
             
     if recieve_servo_data:
         # example output: {0: 0.245, 2: 1.0}
+        #print("hello2")
         for real_id in recieve_servo_data:
             servo_number = real_id
             new_power = recieve_servo_data[real_id]
@@ -102,7 +118,7 @@ if __name__ == "__main__":
                          args=(default_capabilities, feagi_settings, camera_data['vision'],),
                          daemon=True).start()
     
-    model = mujoco.MjModel.from_xml_path('Enter local path for humanoid.xml (found in README.md)')
+    model = mujoco.MjModel.from_xml_path('/Users/ctd/Downloads/humanoid-1.xml')
     data  = mujoco.MjData(model)
     actuators.start_servos(capabilities) # inserted here. This is not something you should do on your end. I will fix it shortly
     with mujoco.viewer.launch_passive(model, data) as viewer:
@@ -124,6 +140,8 @@ if __name__ == "__main__":
             positions = data.qpos #all positions
             positions = positions[7:] #don't know what the first 7 positions are, but they're not joints so ignore them
 
+            abdomen_positions = positions[:3] #first 3 are abdomen z,y,x
+            abdomen_positions = abdomen_positions[::-1] #reverse it to x,y,z order
             """ for i, pos in enumerate(positions):
                 
                 print("[", i, "]", joints[i] ,f": {pos:{.3}g}") """
@@ -145,12 +163,15 @@ if __name__ == "__main__":
                 action(obtained_signals, capabilities)
 
             # Example to send data to FEAGI. This is basically reading the joint. R
+
+            abdomen_gyro_data = {i: pos for i, pos in enumerate(abdomen_positions) if
+                          pns.full_template_information_corticals}
             servo_data = {i: pos for i, pos in enumerate(positions[:20]) if
                           pns.full_template_information_corticals}
-            message_to_feagi = sensors.create_data_for_feagi('servo_position',
+            message_to_feagi = sensors.create_data_for_feagi('gyro',
                                                              capabilities,
                                                              message_to_feagi,
-                                                             current_data=servo_data,
+                                                             current_data=abdomen_gyro_data,
                                                              symmetric=True)
 
             # Sends to feagi data
